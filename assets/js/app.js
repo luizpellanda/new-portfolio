34bb77627adf12a88377224c5278b3ab49f574ff
luizpellanda <108369946+luizpellanda@users.noreply.github.com>
2023-02-02 13:50:46 -0300

    Section footer adicionada - correcao no JS da funcao para enviar email pela secao contatos

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,3 +14,23 @@ menuMobileIcon.addEventListener('click', () => {
     }
 });
 
+//Contact
+
+const sendMailButton = document.getElementById('send-mail-button');
+sendMailButton.addEventListener('click', () => SendMail())
+
+function SendMail () {
+    var params = {
+        from_name: document.getElementById('name').value,
+        email_id: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    }
+    emailjs.send('service_5nabdfz', 'template_um23u9m', params).then(function(res) {
+        if (res.status === 200) {
+            alert(`Thank you for your contact! I will get in touch as soon as possible!`)
+        }
+    })
+}
+
+
+
