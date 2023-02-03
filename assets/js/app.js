2e17871e3d0eff3553f51a01239c3072047870a2
luizpellanda <108369946+luizpellanda@users.noreply.github.com>
2023-02-03 17:51:51 -0300

    Adicionada seção skills

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,23 @@ menuMobileIcon.addEventListener('click', () => {
     }
 });
 
+//CSS Animations
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+       console.log(entry);
+       if (entry.isIntersecting) {
+        entry.target.classList.add('show')
+       } else { //this part is to show the animation more than one time
+        entry.target.classList.remove('show');    
+       }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+
+
 //Contact
 
 const sendMailButton = document.getElementById('send-mail-button');
